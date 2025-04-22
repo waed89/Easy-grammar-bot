e3a5db55091cbff8d6ce0915c78473096367c98c
waed89 <alhoshani89@gmail.com>
2025-04-22 08:10:12 +0300

    Create grammar_data.py

---
+++ grammar_data.py
@@ -0,0 +1,38 @@
+# grammar_data.py
+
+grammar_db = {
+    "past simple": {
+        "explanation": "نستخدم past simple لما نتكلم عن شيء صار وانتهى بالماضي. غالبًا نستخدم أفعال منتظمة أو غير منتظمة.",
+        "examples": [
+            "I watched a movie yesterday.",
+            "She visited her grandma last week.",
+            "They played football after school."
+        ],
+        "quiz": {
+            "question": "اختاري الجملة الصحيحة:",
+            "options": [
+                "He go to school.",
+                "He goes to school.",
+                "He went to school."
+            ],
+            "correct_answer": "He went to school."
+        }
+    },
+    "present simple": {
+        "explanation": "نستخدم present simple لما نتكلم عن أشياء تحصل دائمًا أو عادةً.",
+        "examples": [
+            "She drinks coffee every morning.",
+            "They play soccer on Fridays.",
+            "The sun rises in the east."
+        ],
+        "quiz": {
+            "question": "وش الجملة الصحيحة؟",
+            "options": [
+                "She go to school.",
+                "She goes to school.",
+                "She going to school."
+            ],
+            "correct_answer": "She goes to school."
+        }
+    }
+}
